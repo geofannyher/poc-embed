9597b2b6242765809003b535390edd469f20331b
kobars <kobar.sept@gmail.com>
2024-12-04 16:14:05 +0700

    Update script source URLs to use production endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Script
-        src="http://localhost:3000/embed.js"
-        data-src="http://localhost:3000"
+        src="https://poc-embed-remote.vercel.app/embed.js"
+        data-src="https://poc-embed-remote.vercel.app"
         strategy="lazyOnload"
       />
     </div>
